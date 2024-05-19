8860cb0cc5a1a4b89e7045068d681162c2de1d30
bigspotteddog <bink.lynch@gmail.com>
2024-05-18 19:48:20 -0700

    Editor and preview works.

---
+++ src/main/java/com/example/blogeditor/BlogEditorApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogEditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blogeditor/BlogEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
